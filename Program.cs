a197c2ab0ca792e10a839213a4461a6d7acfb678
yhalk <yhalk@PF2FK1NN>
2022-04-14 16:49:46 +0300

    123

--- Program.cs
+++ Program.cs
@@ -11,15 +11,15 @@ namespace Exercise1
         static void Main(string[] args)
         {
             Console.WriteLine("Hi. What is your name?");
-            String name = Console.ReadLine();
+            string name = Console.ReadLine();
 
             Console.WriteLine("Hi. What is your year of birth?");
-            String dateOfBirth = Console.ReadLine();
+            string dateOfBirth = Console.ReadLine();
 
             int age = 2022 - Convert.ToInt32(dateOfBirth);
             Console.WriteLine("Hi! Your name is: {0} and your age is {1}", name, age);
 
-            String replace = Console.ReadLine();
+            string replace = Console.ReadLine();
             Console.WriteLine(replace.Replace(" ", "_"));
         }
     }
